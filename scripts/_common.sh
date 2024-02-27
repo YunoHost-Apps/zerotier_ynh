b20e5782e68c5423a14dcb829277c9b74d2c541f
Félix Piédallu <felix@piedallu.me>
2024-02-27 14:47:32 +0100

    Manifest v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-version_codename=$(awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release)
-
 #=================================================
 # COMMON VARIABLES
 #=================================================
-
-ZT_BASE_URL_HTTPS='https://download.zerotier.com/'
-ZT_BASE_URL_HTTP='http://download.zerotier.com/'
-service="zerotier-one"
-
-# dependencies used by the app
-pkg_zerotier="zerotier-one"
-pkg_version="1.12.2"
 
 #=================================================
 # PERSONAL HELPERS
